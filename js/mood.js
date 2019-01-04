aea4b8050503629d3fb5ab215d70167ab945fc2d
Magdalena Idzik <midzik@consol.de>
2019-01-04 11:33:34 +0100

    Mood stats

--- js/mood.js
+++ js/mood.js
@@ -15,7 +15,13 @@ var resetFeedback = function() {
   rate.style.display = "inherit";
   feedback.style.display = "none";
 }
-var stats;
+var stats = {
+  "terrible": 0,
+  "ok": 0,
+  "good": 0,
+  "awesome": 0
+};
+
 var getData = function() {
   fetch('https://shipit-d1d3.restdb.io/rest/feedback', {headers: {"x-apikey": "5c2e8ccd66292476821c9cb3"}})
   .then(function(response) {
@@ -23,33 +29,41 @@ var getData = function() {
    })
    .then(function(myJson) {
      console.log(JSON.stringify(myJson));
-     stats = myJson[0];
-     updateStats();
+     updateStats(myJson);
    });
 }
-var updateStats = function() {
-  terrible.innerHTML = stats["terrible"];
-  ok.innerHTML = stats["ok"];
-  good.innerHTML = stats["good"];
-  awesome.innerHTML = stats["awesome"];
+
+var updateStat = function(type) {
+  return fetch('https://shipit-d1d3.restdb.io/rest/feedback?q={"'+type+'": true}', {headers: {"x-apikey": "5c2e8ccd66292476821c9cb3"}})
+  .then(function(response) {
+     return response.json();
+   })
+   .then(function(myJson) {
+     stats[type] = myJson.length;
+     return stats[type];
+   });
+}
+var updateStats = function(response) {
+  updateStat("terrible").then(response => {terrible.innerHTML = response;});
+  updateStat("ok").then(response => {ok.innerHTML = response;});
+  updateStat("good").then(response => {good.innerHTML = response;});
+  updateStat("awesome").then(response => {awesome.innerHTML = response});
 }
 var postData = function (opinion) {
   var data = {}
-  data[opinion] = stats[opinion] == undefined ? 1 : parseInt(stats[opinion]) + 1;
-
-  var uri = "https://shipit-d1d3.restdb.io/rest/feedback/" + stats._id;
+  //data[opinion] = stats[opinion] == undefined ? 1 : parseInt(stats[opinion]) + 1;
+  data[opinion] = true;
+  var uri = "https://shipit-d1d3.restdb.io/rest/feedback";
   fetch(uri, {
-  method: 'PUT', // or 'PUT'
+  method: 'POST', // or 'PUT'
   body: JSON.stringify(data), // data can be `string` or {object}!
   headers:{
     'Content-Type': 'application/json',
     "x-apikey": "5c2e8ccd66292476821c9cb3"
   }
-}).then(res => res.json())
+}).then(res => {updateStats(); return res.json();})
 .then(function(response){
   console.log('Success:', JSON.stringify(response));
-  stats = response[0];
-  //updateStats();
 })
 .catch(error => console.error('Error:', error));
 }
